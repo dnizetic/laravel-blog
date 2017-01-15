6de273704900c158faf2225f5c05d82853f01564
dnizetic <denis_nizetic@hotmail.com>
2017-01-15 01:29:18 +0100

    Updates

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -52,6 +52,7 @@ use AuthenticatesAndRegistersUsers,
     protected function validator(array $data) {
         return Validator::make($data, [
                     'name' => 'required|max:255',
+                    'last_name' => 'required|max:255',
                     'email' => 'required|email|max:255|unique:users',
                     'password' => 'required|min:6|confirmed',
         ]);
@@ -66,12 +67,12 @@ use AuthenticatesAndRegistersUsers,
     protected function create(array $data) {
         return User::create([
                     'name' => $data['name'],
+                    'last_name' => $data['last_name'],
                     'email' => $data['email'],
                     'password' => bcrypt($data['password']),
         ]);
     }
 
-    
     /**
      * This method ensures user is not logged in unless activated
      * @param Request $request
@@ -87,7 +88,6 @@ use AuthenticatesAndRegistersUsers,
         return redirect()->intended($this->redirectPath());
     }
 
-    
     /**
      * Activate user
      * @param type $token
